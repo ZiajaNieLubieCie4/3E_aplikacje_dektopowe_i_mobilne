71ed27c5597f511fb9b13e0a4afd0bf431fa9278
ZiajaNieLubieCie4 <145669961+ZiajaNieLubieCie4@users.noreply.github.com>
2024-10-02 13:53:56 +0200

    Add files via upload

---
+++ 02.10.2024/zad1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Reflection.Emit;
+
+public class program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Podaj x");
+        double x = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Podaj y");
+        double y = Convert.ToDouble(Console.ReadLine());
+
+        string c;
+
+        if (x > 0 && y > 0)
+        {
+            c = ("Lewy gorny");
+        }
+        else if (x > 0 && y > 0)
+        {
+            c = ("Prawy gorny");
+        }
+        else if (x < 0 && y < 0)
+        {
+            c = ("Lewy dolny");
+        }
+        else if (x > 0 && y < 0)
+        {
+            c = ("Prawy dolny");
+        }
+        else
+        {
+            c = "Linia wykresu";
+        }
+
+        Console.WriteLine(c);
+    }
+}
---
+++ 02.10.2024/zad2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Podaj a");
+        int a = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Podaj b");
+        int b = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Podaj c");
+        int c = Convert.ToInt32(Console.ReadLine());
+
+        double obwod = a + b + c;
+        double pole = (0.25 * (Math.Sqrt(4 * a * a * b * b - (a * a + b * b - c * c) * (a * a + b * b - c * c))));
+
+        if (b + c > a && b + a > c && c > b)
+        {
+            Console.WriteLine("Da sie zbudowac trójkąt");
+        }
+        else
+        {
+            Console.WriteLine("Nie da sie zbudować trójkąta");
+        }
+        Console.WriteLine("Pole " + pole);
+        Console.WriteLine("Obowod " + obwod);
+    }
+}
